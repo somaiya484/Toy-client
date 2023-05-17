4b4e4e4aa735e6c3f48a941ad34954a09e25c57b
somaiya shahin <somaiyashahin484@gmail.com>
2023-05-18 01:01:35 +0600

    navbar and footer added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../allPages/Others/Footer/Footer';
+import Navbar from '../allPages/Others/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
-    );
+    );    
 };
 
 export default Main;
