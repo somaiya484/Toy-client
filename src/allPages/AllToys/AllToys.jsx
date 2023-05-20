9045407abfb60424e5fb457e20daff2e5d8cdf7b
somaiya shahin <somaiyashahin484@gmail.com>
2023-05-20 18:42:35 +0600

    added search by handle function

--- src/allPages/AllToys/AllToys.jsx
+++ src/allPages/AllToys/AllToys.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 const AllToys = () => {
     const [toys, setToys] = useState([]);
+    const [searchToy, setSearchToy] = useState("")
+
 
     useEffect(() => {
         fetch('http://localhost:5000/allToys')
@@ -9,10 +11,26 @@ const AllToys = () => {
             .then(result => {
                 setToys(result)
             })
-    }, [])
+    }, []);
+
+
+    const searchHandle = () => {
+        fetch(`http://localhost:5000/getToysBySearch/${searchToy}`)
+            .then(res => res.json())
+            .then(data => {
+                setToys(data)
+            })
+    }
 
     return (
         <div className="overflow-x-auto px-6 py-12 bg-purple-50">
+            <div className="search-box p-2 text-center">
+                <input className="border-purple-500 border-2 py-3 px-4 m-2 rounded " placeholder="Toy Name"
+                    type="text"
+                    onChange={(event) => setSearchToy(event.target.value)}
+                />{" "}
+                <button onClick={searchHandle} className=" py-1 px-3 border-purple-700 border-2 bg-purple-700 text-white text-sm text-center m-10 rounded mt-4 ml-1 cursor-pointer hover:bg-transparent hover:text-black hover:border-purple-700 ">Search</button>
+            </div>
             <table className="table table-compact w-full">
                 <thead className="text-center ">
                     <tr>
