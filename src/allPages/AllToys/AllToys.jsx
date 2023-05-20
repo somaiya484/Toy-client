aa0e29dd6cd1b26c48e28b05ea1f75903648d8c3
somaiya shahin <somaiyashahin484@gmail.com>
2023-05-20 15:13:06 +0600

    fixed all toys table

--- src/allPages/AllToys/AllToys.jsx
+++ src/allPages/AllToys/AllToys.jsx
@@ -12,29 +12,29 @@ const AllToys = () => {
     }, [])
 
     return (
-        <div className="overflow-x-auto px-6 py-12">
+        <div className="overflow-x-auto px-6 py-12 bg-purple-50">
             <table className="table table-compact w-full">
-                <thead className="text-center">
+                <thead className="text-center ">
                     <tr>
-                        <th>Seller</th>
-                        <th>Toy Name</th>
-                        <th>Toy category</th>
-                        <th>Price</th>
-                        <th>Available Quantity</th>
-                        <th></th>
+                        <th className="bg-purple-200 py-5">Seller</th>
+                        <th className="bg-purple-200">Toy Name</th>
+                        <th className="bg-purple-200">Toy category</th>
+                        <th className="bg-purple-200">Price</th>
+                        <th className="bg-purple-200">Available Quantity</th>
+                        <th className="bg-purple-200"></th>
 
                     </tr>
                 </thead>
                 {
                     toys.map(toy => (
-                        <tbody key={toy.id} className="text-center">
+                        <tbody key={toy.id} className="text-center px-3">
                             <tr>
-                                <td>{toy.sellerEmail}</td>
+                                <td>{toy?.userName}</td>
                                 <td>{toy.toyName}</td>
-                                <td>{toy.toyName}</td>
-                                <td>{toy.userName}</td>
+                                <td>{toy.status}</td>
                                 <td>{toy.price}</td>
-                                <td><button className=" bg-purple-700 text-white font-normal hover:bg-transparent hover:text-black hover:border-purple-700 hover:border-2 py-1 px-3 rounded
+                                <td>{toy.quantity}</td>
+                                <td><button className=" bg-purple-700 border-2 border-purple-700  text-white font-normal hover:bg-transparent hover:text-black hover:border-purple-700 hover:border-2 py-1 px-3 rounded
                                 ">View Details</button></td>
                             </tr>
                         </tbody>
